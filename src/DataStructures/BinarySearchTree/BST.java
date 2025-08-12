03d2a37b4efc499d2c5efd983894c07bbc84d104
ashankdavid <ashank2507david@gmail.com>
2025-08-12 11:52:26 +0530

    Just added BST Insert

---
+++ src/DataStructures/BinarySearchTree/BST.java
@@ -0,0 +1,68 @@
+package DataStructures.BinarySearchTree;
+
+class Node{
+    int key;
+    Node left;
+    Node right;
+
+    Node(int key){
+        this.key = key;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class BST {
+    Node root;
+
+    BST(){
+        root = null;
+    }
+
+    private Node insertRec(Node root, int key){ // Helper Function for Insert
+        if(root == null){
+            return new Node(key);
+        }
+        if(key < root.key){
+            root.left = insertRec(root.left, key);
+        }else if(key > root.key){
+            root.right = insertRec(root.right, key);
+        }return root;
+    }
+
+    public void insert(int key){
+        root = insertRec(root, key);
+    }
+
+    public void search(){
+
+    }
+
+    public void remove(){
+
+    }
+
+    void dfs(Node root){
+        if(root==null) {return ;}
+        dfs(root.left);
+        System.out.print(root.key + " ");
+        dfs(root.right);
+    }
+}
+
+class DriverCode{
+    public static void main(String[] args) {
+        BST bst = new BST();
+        bst.insert(100);
+        bst.insert(50);
+        bst.insert(150);
+        bst.insert(30);
+        bst.insert(60);
+        bst.insert(55);
+        bst.insert(120);
+        bst.insert(110);
+        bst.insert(140);
+        bst.insert(180);
+        bst.dfs(bst.root);
+    }
+}
