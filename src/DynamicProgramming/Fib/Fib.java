a3359c9e0229bcafce7a19c9b2bc1de7927a1d16
ashankdavid <ashank2507david@gmail.com>
2025-08-14 12:33:49 +0530

    Just added the Fib Memoization DP program

---
+++ src/DynamicProgramming/Fib/Fib.java
@@ -0,0 +1,21 @@
+package DynamicProgramming.Fib;
+
+public class Fib {
+    static int fib(int x, int[] dp){
+        if(x==0 || x==1){
+            return x;
+        }
+        if(dp[x]!=0){
+            return dp[x];
+        }
+        dp[x] = fib(x-1, dp) + fib(x-2, dp);
+        return dp[x];
+
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int[] dp = new int[n + 1];
+        int result = fib(n, dp);
+    }
+}
